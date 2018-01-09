823ea1f270a8951fe7e776c16973241c814a9535
Jeff Kaufman <jeff@jefftk.com>
2018-01-09 18:56:38 +0000

    updates; fix geocoding

--- lookup_locs.py
+++ lookup_locs.py
@@ -3,11 +3,11 @@ from json import encoder
 import urllib2
 import time
 
-KEY="AIzaSyAIprYkqDGIH1EQ1ssy8Ef1q-8U7Hdk6Nc"
+KEY="AIzaSyCuMCzvNjdpzYJMFR8BWmbGzO68HbHPkGA"
 
 def lookup_ll(loc):
   loc = loc.replace(" ", "+")
-  q = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (loc, KEY)
+  q = "https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s" % (loc, KEY)
   j = urllib2.urlopen(q).read()
   r = json.loads(j)
 
@@ -16,7 +16,7 @@ def lookup_ll(loc):
   except Exception:
     import pprint
     pprint.pprint(r)
-    return 0,0
+    raise
 
   return ll["lat"], ll["lng"]
 
