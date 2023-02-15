ea2cc5ec7f2f9e11658fc7ab0dbef33609e76626
Jeff Kaufman <jeff@jefftk.com>
2023-02-14 22:12:39 -0500

    refresh zipcodes

--- build_zipcode_json.py
+++ build_zipcode_json.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+
 import sys
 import json
 import csv
 
 def start(zip_csv):
   data = {}
-  with open(zip_csv, 'rb') as inf:
+  with open(zip_csv, 'r') as inf:
     for i, row in enumerate(csv.reader(inf)):
       zipcode = row[0]
       lat = row[12]
@@ -14,6 +16,8 @@ def start(zip_csv):
         assert zipcode == "zip"
         assert lat == "latitude"
         assert lng == "longitude"
+      elif lat == "0" and lng == "0":
+        continue
       else:
         data[zipcode] = [float(lat), float(lng)]
   with open("zipcode.json", 'w') as outf:
