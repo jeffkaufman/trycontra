0a86a8f8ba63c81509d156c9d1ab2d0d40c810cb
Jeff Kaufman <jeff@jefftk.com>
2023-06-12 21:23:03 -0400

    more 2024 import

--- process-events.py
+++ process-events.py
@@ -3,66 +3,71 @@ import re
 import json
 
 records = []
-with open("events-raw-2023.tsv") as inf:
-    for n, line in enumerate(inf):
-        if n == 0:
-            continue
+for year, fname in [
+        (2023, "events-raw-2023.tsv"),
+        (2024, "events-raw-2024.tsv"),
+]:
+    with open(fname) as inf:
+        for n, line in enumerate(inf):
+            if n == 0:
+                continue
 
-        (typical_month,
-         name,
-         caller1,
-         caller2,
-         caller3,
-         caller4,
-         caller5,
-         caller6,
-         band1,
-         band2,
-         band3,
-         band4,
-         band5,
-         band6,
-         roles,
-         date,
-         location,
-         url,
-         *_) = line.split("\t")
+            (typical_month,
+             name,
+             caller1,
+             caller2,
+             caller3,
+             caller4,
+             caller5,
+             caller6,
+             band1,
+             band2,
+             band3,
+             band4,
+             band5,
+             band6,
+             roles,
+             date,
+             location,
+             url,
+             *_) = line.split("\t")
 
 
-        if not name:
-            continue
+            if not name:
+                continue
 
-        if name == "end of active list":
-            break
+            if name == "end of active list":
+                break
 
-        callers = [x for x in (caller1,
-                               caller2,
-                               caller3,
-                               caller4,
-                               caller5,
-                               caller6)
-                   if x.strip()]
+            callers = [x for x in (caller1,
+                                   caller2,
+                                   caller3,
+                                   caller4,
+                                   caller5,
+                                   caller6)
+                       if x.strip()]
 
-        bands = [re.sub("^The ", "the ", x)
-                 for x in (band1,
-                             band2,
-                             band3,
-                             band4,
-                             band5,
-                             band6)
-                 if x.strip() and x.lower() != "unnamed"]
+            bands = [re.sub("^The ", "the ", x)
+                     for x in (band1,
+                               band2,
+                               band3,
+                               band4,
+                               band5,
+                               band6)
+                     if x.strip() and x.lower() != "unnamed"]
 
-        records.append(
-            {"typical_month": typical_month,
-             "name": name,
-             "callers": callers,
-             "bands": bands,
-             "roles": roles,
-             "date": date,
-             "location": location,
-             "url": url,
-             }
-        )
+            records.append(
+                {"typical_month": typical_month,
+                 "name": name,
+                 "callers": callers,
+                 "bands": bands,
+                 "roles": roles,
+                 "date": date,
+                 "location": location,
+                 "url": url,
+                 "year": year,
+                 }
+            )
 
 with open("events.json") as inf:
     old_records = json.load(inf)
