3ac3e72788f0045c6fa019c1daebcad02e49be6d
Jeff Kaufman <jeff@jefftk.com>
2023-11-16 09:31:27 -0500

    sync spreadsheet: hardcode the url, don't clean up line endings, be less noisy

--- sync-spreadsheet.sh
+++ sync-spreadsheet.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# Usage: ./sync-spreadsheet.sh <Google Sheet key>
-# The Google Sheet key is a 44-character long alphanumeric string which you can get from the URL of
-# the spreadsheet.
+# Usage: ./sync-spreadsheet.sh
 
 set -e
 
 git pull
 
+sheet_key="1fQq7pTtNVMYVRgOPbjNz2jnyw4RABGrQoplrSQntbn8"
+sheet_url="https://docs.google.com/spreadsheets/d/$sheet_key/gviz/tq?tqx=out:csv"
 for i in 2017 2018 2019 2023 2024; do
-    wget "https://docs.google.com/spreadsheets/d/$1/gviz/tq?tqx=out:csv&sheet=$i" -O - \
-    | csvformat -T > events-raw-$i.tsv
-    unix2dos events-raw-$i.tsv
+    curl -sS "${sheet_url}&sheet=$i" | csvformat -T > events-raw-$i.tsv
 done
 
 if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
