10b93adebc6182d5a4b91ebf3bdef482409257a5
Jeff Kaufman <jeff@jefftk.com>
2025-03-01 12:38:44 -0500

    update which bands use the

--- process-events.py
+++ process-events.py
@@ -4,32 +4,65 @@ import glob
 import json
 
 bands_add_the = [
+    "O'Schraves",
     'Adobe Brothers',
     'Avant Gardeners',
+    'Buzz Band',
     'Canote Brothers',
+    'Cheap Shots',
+    'Contra Rebels',
     'Contrarians',
+    'Cosmic Otters',
+    'Crabapples',
     'Dam Beavers',
+    'Dead Sea Squirrels',
+    'Dixie Butterhounds',
+    'Dog Walkers',
+    'Electrodes',
     'Engine Room',
+    'FIgments',
     'Faux Paws',
+    'Fiddle Hellions',
     'Figments',
     'Free Raisins',
     'Gaslight Tinkers',
+    'Hat Band',
+    'Hollertones',
+    'Ice Cream Truckers',
     'JEMS',
+    'Jig Lords',
+    'Johns',
     'Latter Day Lizards',
+    'Luddite Ramblers',
     'Mean Lids',
+    'Midlings',
     'Moving Violations',
-    "O'Schraves",
+    'O-Tones',
     'Offbeats',
+    'Old-Time Superstars',
+    'Olympia Volunteer String Band',
+    'Orphans',
+    'Pegasus Collective',
     'Quarks',
+    'Reckoners',
     'Red Mountain Yellowhammers',
+    'Resurrection Marys',
     'Rhythm Raptors',
     'Ripples',
+    'Sage Thrashers',
+    'Sail Away Ladies',
     'Stringrays',
     'Stuff',
     'Syncopaths',
+    'Tricky Brits',
+    'Tune Drivers',
     'Turning Stile',
+    'Virginia Creepers',
     'Whoots',
+    'Yellow Dandies',
 ]
+
+possible_the_bands = set()
 
 records = []
 for fname in sorted(glob.glob("events-raw-*.tsv")):
@@ -106,6 +139,11 @@ for fname in sorted(glob.glob("events-raw-*.tsv")):
                                band6)
                      if x.strip() and x.lower() != "unnamed"]
 
+            for band in bands:
+                if band.startswith("the ") and \
+                   band.removeprefix("the ") not in bands_add_the:
+                    possible_the_bands.add(band)
+
             bands = [
                 "the " + band if band in bands_add_the else band
                 for band in bands]
@@ -133,6 +171,11 @@ for fname in sorted(glob.glob("events-raw-*.tsv")):
             print(line)
             raise
 
+if possible_the_bands:
+    print("Consider adding to the 'bands_add_the' list:")
+    for possible_the_band in sorted(possible_the_bands):
+        print("    %r," % possible_the_band.removeprefix("the "))
+        
 with open("events.json") as inf:
     old_records = json.load(inf)
 
