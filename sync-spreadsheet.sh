85500a9c08dfc2d40983076aca4d74bc91573e20
Jeff Kaufman <jeff@jefftk.com>
2025-07-26 08:56:50 -0400

    rework sheet downloads

--- sync-spreadsheet.sh
+++ sync-spreadsheet.sh
@@ -3,13 +3,30 @@
 # Usage: ./sync-spreadsheet.sh
 
 set -e
+set -o pipefail
 
 git pull
 
+declare -A sheet_gids=(
+    ["2016"]="1649588387"
+    ["2017"]="602090070"
+    ["2018"]="1899461454"
+    ["2019"]="1075288908"
+    ["2023"]="39088556"
+    ["2024"]="53541576"
+    ["2025"]="1187858755"
+    ["2026"]="465816395"
+)
+
+
 sheet_key="1fQq7pTtNVMYVRgOPbjNz2jnyw4RABGrQoplrSQntbn8"
-sheet_url="https://docs.google.com/spreadsheets/d/$sheet_key/gviz/tq?tqx=out:csv"
-for i in 2016 2017 2018 2019 2023 2024 2025 2026; do
-    curl -sS "${sheet_url}&sheet=$i" | csvformat -T > events-raw-$i.tsv
+url="https://docs.google.com/spreadsheets/d/${sheet_key}/export?format=csv"
+
+for year in "${!sheet_gids[@]}"; do
+    gid="${sheet_gids[$year]}"
+
+    echo "Downloading sheet $year (gid: $gid)..."
+    curl -L -sS "$url&gid=${gid}" | csvformat -T > "events-raw-$year.tsv"
 done
 
 if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
